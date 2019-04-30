9f2e05e9ff4710048033fafa85aa00963655cd40
Eugene <podbereznykh@mail.ru>
2019-04-30 21:09:20 +0500

    Урок 2

--- script2.js
+++ script2.js
@@ -1,27 +1,79 @@
+// Урок 2
+
+let money = +prompt("Ваш бюджет на месяц?",'0'),
+    time = prompt("Введите дату в формате YYYY-MM-DD",'2019-04-30');
+
+let appData = {
+    budjet: money, 
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let i = 0;
+// FOR
+for ( 0; i < 2; i++ ) {
+
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');
+
+        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        console.log("Done"); 
+        appData.expenses[a] = b;
+    } else {
+        alert("Данные введены с ошибкой. Повторите ввод.");
+        --i; continue;
+    } 
+} 
+
+/* 
+// WHILE
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+        i++;
+    } else {
+        alert("Данные введены с ошибкой. Повторите ввод.");   
+    }
+} 
+*/
+
+
+/*
+// DO
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+    b = prompt("Во сколько обойдется?",'');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+    appData.expenses[a] = b;
+    i++;
+    } else {
+    alert("Данные введены с ошибкой. Повторите ввод.");   
+    }
+} while (i < 2);
+*/
+
+appData.moneyPerDay = appData.budjet / 30;
+alert("Ежемесячный бюджет: " + appData.moneyPerDay);
+console.log(appData);
+
+let m = appData.moneyPerDay; // для удобства
+if (m < 100) {
+    console.log("Минимальный уровень достатка.");
+} else if (m > 100 && m < 2000 ) {
+    console.log("Средний уровень достатка.");
+} else if (m > 2000) {
+    console.log("Высокий уровень достатка.");
+} else {
+    console.log("Произошла ошибка.");
+}
+
 //Вопросы к этому заданию
-//Какое будет выведено значение: let x = 5; alert( x++ ); ?
-
-//Чему равно такое выражение: [ ] + false - null + true ?
-
-//Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
-
-//Чему равна сумма [ ] + 1 + 2?
-
-//Что выведет этот код: alert( "1"[0] )?
-
-//Чему равно 2 && 1 && null && 0 && undefined ?
-
-//Есть ли разница между выражениями? !!( a && b ) и (a && b)?
-
-//Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
-console.log(null || 2 && 3 || 4 );
-//a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
-//a = [1, 2, 3]; 
-//b = [1, 2, 3];
-//console.log(a === b);
-//Что выведет этот код: alert( +"Infinity" ); ?
-//alert( +"Infinity" );
-//Верно ли сравнение: "Ёжик" > "яблоко"?
-//console.log("Ёжик" > "яблоко");
-//Чему равно 0 || "" || 2 || undefined || true || falsе ? 
-//console.log(0 || "" || 2 || undefined || true || falsе);
+//Сколько видов циклов существует в JS?
+// Ответ: 3 (for, while и do)
